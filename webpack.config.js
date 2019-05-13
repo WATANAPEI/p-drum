2fa15764534b6959494c52787cd3869fe31e6038
WATANAPEI <pei2791150@gmail.com>
2019-05-13 16:29:14 +0200

    enable https at webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 const path = require('path');
+const fs = require('fs');
 
 module.exports = {
     mode: 'production',
@@ -9,16 +10,22 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, './out'),
+        publicPath: "/p-drum/",
         filename: 'bundle.js'
     },
     devtool: 'eval-source-map',
     devServer: {
-      contentBase: path.resolve(__dirname, './client'),
+      contentBase: path.resolve(__dirname, './out'),
       port: 8080,
       compress: true,
       inline: true,
       disableHostCheck: true,
-      host: "0.0.0.0"
+      host: "0.0.0.0",
+      https: {
+          key: fs.readFileSync('/etc/ssl/certs/localhost-key.pem'),
+          cert: fs.readFileSync('/etc/ssl/certs/localhost-crt.pem'),
+          ca: fs.readFileSync('/etc/nginx/local_root_CA/rootCA.pem')
+      }
     },
     resolve: {
       modules: [path.resolve(__dirname, "client"), "node_modules"],
